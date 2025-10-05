c208ad4e507a532dc3f84142afb786ac20dc6032
zivdmagdy <ziadmagdy109@yahoo.com>
2025-10-05 09:38:03 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:notes/my_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:notes/views/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
